c1b3f76f6d4ccbeb1aaffb3f0dc2725413a7fe3f
Onur <onurkordogan@gmail.com>
2022-12-10 21:34:04 +0300

    datatypes

---
+++ 02_Datatypes-operators/app.js
@@ -0,0 +1,25 @@
+// let a;
+// console.log(a);
+// console.log(b);
+// console.log('hello');
+
+// console.log(a);
+// var a = 3;
+// var aa = 2;
+// console.log(a);
+
+// console.log(b);
+// let b = 1;
+// console.log(b);
+
+// console.log(c);
+// const c = 4;
+
+// let c = "2";
+// let d = 2;
+// console.log(typeof c);
+// console.log(typeof d);
+
+// let e = +prompt("bir şey yazın"); //her zaman string döndürür, başına artı(+) koyarsak number döndürür.
+// console.log(typeof e); 
+
