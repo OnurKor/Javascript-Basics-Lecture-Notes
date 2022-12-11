a52633aa8b584a5ea0c9437a7057634db0769681
Onur <onurkordogan@gmail.com>
2022-12-11 22:59:13 +0300

    loopsstart

---
+++ 04_Loops/loops.js
@@ -0,0 +1,54 @@
+console.log('****** LOOPS *******');
+
+// * =======================================================
+// *                     DÖNGÜLER
+// * =======================================================
+
+//************************ FOR ****************************
+// Math.floor() =>aşağıya yuvarlar
+// Math.ceil() =>sürekli yukarıya yuvarlar
+// Math.round() => değerine göre yuvarlar.
+// Math.trunc() => kesirli sayının tam kısmı alır.
+
+
+
+
+//? Ornek: 1 den n kadar sayıları toplayan kodu yazınız.
+
+
+// //? ÖRNEK: Dışarıdan girilen sayının Asal olup olmadığını tespit ederek sonucu yazdıran programı yazınız.
+
+// //******************** WHILE ******************
+
+
+
+// //* Ornek
+
+// //******************** DO-WHILE ******************
+// let sayac = 1;
+
+// do {
+//   console.log('Merhaba-', sayac);
+//   sayac++;
+// } while (sayac <= 5);
+
+// let not2;
+
+// do {
+//   not2 = prompt('Lütfen notunuzu giriniz (0-100):');
+//   if (not2 < 0 || not2 > 100) {
+//     alert('Girdiğiniz not 0-100 arasında olmalıdır');
+//   }
+// } while (not2 < 0 || not2 > 100);
+
+// console.log('Girdiğiniz not', not2);
+
+// //? ODEV1: klavyeden Q karakteri girilene kadar not girişi yapan bir programı döngü kullanarak yazınız.
+
+// let not3;
+
+// while (not3 != 'q') {
+//   not3 = prompt('Lütfen not giriniz veya Q ya basarak çıkınız!').toLowerCase();
+// }
+
+// console.log('Q ya basarak çıkış yaptınız !!!');
