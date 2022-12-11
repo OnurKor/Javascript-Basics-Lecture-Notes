755623f547c5c4b0def01cdd4f74ec89cbfc5fc2
Onur <onurkordogan@gmail.com>
2022-12-11 10:15:44 +0300

    Condition-start

---
+++ 03_Conditions/app.js
@@ -0,0 +1,108 @@
+// console.log('**********\nSession 3 - Conditionals\n**********');
+// ! if statement example
+// check if the is positive
+
+
+
+// ! example 2 chest size
+
+// https://www.perryellis.com/pages/size-chart
+
+// suggest size for men as XS S M L XL XXL on chest size
+
+
+
+// ! example 2 three number comparison
+
+// Write a JavaScript program that accept three integers and display the greatest.
+
+// n1 is the greatest among n2 and n3
+// 8 is the g     among 3 and 5
+
+
+// ! ternary examples
+// console.log('----------\nternary if statements\n----------');
+
+// program to check pass or fail
+// let grade = prompt('Enter your score :');
+
+// ! evde denemeyiniz !!
+
+// ! example-4 canDrive
+
+// evaluate if a human can drive a car or not
+
+// let canDrive = false;
+// const hasDriversLicense = false;
+// const hasGoodVision = false;
+
+// const isHandicapped = false;
+// const usingRegularCar = false;
+// // // const usingDisabledCar = true;
+
+// let canDrive = false;
+// const hasDriversLicense = true;
+// const hasGoodVision = true;
+// const isHandicapped = false;
+// const usingRegularCar = true;
+
+// result =
+//   hasDriversLicense && hasGoodVision
+//     ? !isHandicapped
+//       ? 'can drive'
+//       : !usingRegularCar
+//       ? 'can drive'
+//       : 'cannot drive'
+//     : 'cannot drive';
+// console.log(result);
+
+console.log('----------\nswitch case statements\n----------');
+
+let text;
+// let fruit = prompt('type your favorite fruit');
+// banana, lime, limon, orange, apple, default
+
+// switch (fruit.toLowerCase()) {
+//   case 'banana':
+//     text = 'I like Banana!🍌';
+//     break;
+//   case 'lime':
+//   case 'lemon':
+//   case 'orange':
+//     text = `I am not a fan of ${fruit}`;
+//     break;
+//   case 'apple':
+//     text = `I like 🍎🍏`;
+//     break;
+
+//   default:
+//     text = "I don't know that fruit";
+// }
+
+// console.log(text);
+
+// ! example-5  cw weekly program ❗your turn 🧑‍💻
+// pazartesi salı çarşamba perşembe in class
+// cuma team work
+// cumartesi inclass + workshop
+// pazar self study
+// default yanlış gün girildi.
+
+
+// ! example 5
+// output day count of a month of given year 2020 02 => 29
+// 2022 02 => 28
+
+//? leap year
+
+
+// ! example-6 path selection
+
+/*
+path = "ad", "fs", "ds" // ad => aws-devops , fs u know, ds => data-science
+pathInfo = ""; // provide some information about the selected path.
+*/
+
+// const pathSelected = prompt(
+//   'Which path would you like to attend?\nad -> aws-devops\nfs -> full-stack\nds -> data-science'
+// );
