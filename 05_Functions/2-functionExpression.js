2fb995102c45c1d9c0c10e73c1659453653898f1
Onur <onurkordogan@gmail.com>
2022-12-13 16:10:02 +0300

    sessionfinish

--- 05_Functions/2-functionExpression.js
+++ 05_Functions/2-functionExpression.js
@@ -48,3 +48,36 @@ console.log("En Büyük Sayi:", buyukBul(-8, -9));
 
 //* ORNEK3: Bir fonksiyon içerisinde başka fonksiyonlar çağrılabilir
 //*****************************************************/
+
+const hesapla = function (islem, s1, s2) {
+  let sonuc;
+
+  if (islem == "+") {
+    sonuc = topla(s1, s2);
+  } else if (islem == "-") {
+    sonuc = cikar(s1, s2);
+  } else if (islem == "*") {
+    sonuc = carp(s1, s2);
+  } else if (islem == "/") {
+    sonuc = bol(s1, s2);
+  }
+  return sonuc;
+};
+
+const topla = function (s1, s2) {
+  return s1 + s2;
+};
+const cikar = function (s1, s2) {
+  return s1 - s2;
+};
+const carp = function (s1, s2) {
+  return s1 * s2;
+};
+const bol = function (s1, s2) {
+  return s1 / s2;
+};
+
+console.log(hesapla("+", 2, 3));
+console.log(hesapla("-", 2, 3));
+console.log(hesapla("*", 2, 3));
+console.log(hesapla("/", 2, 3));
