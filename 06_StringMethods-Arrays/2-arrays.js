336b7c320d8446a641b95caf9fef80dae3ebda7f
Onur <onurkordogan@gmail.com>
2022-12-15 20:19:03 +0300

    mutator

--- 06_StringMethods-Arrays/2-arrays.js
+++ 06_StringMethods-Arrays/2-arrays.js
@@ -8,27 +8,50 @@ console.log('***** ARRAYS ******');
 //* ---------------------------------------------------------
 
 // ! 1.Yöntem (Array Literal) - Tercih edilen yöntem
-
+const isimler = ["ahmet", "mehmet", "ismet", "saffet"];
+console.log(isimler);
+console.log(isimler.length); //? 4
 
 // ! 2.Yöntem (Object Constructor)
+const diller = new Array("Python", "Java", "C++", "JS", "Go");
+console.log(diller);
 
-//! 10 elemanlık boş bir array oluşturdu
+const numbers = new Array(3, 2, 1);
+console.log(numbers)
+
+const sayilar = new Array(10);//! 10 elemanlık boş bir array oluşturdu
+console.log(sayilar);
+
+
 
 
 //* Diziden Veri Okuma-Yazma
 //* ---------------------------------------------------------
 
 //!Okuma
-
+const java = diller[1];
+console.log(java);
 
 //!Diziye Yazma
-
+isimler[2] = "Canan"
+console.log(isimler);
 
 //! Hata,const keyword'u ile tanımlanmış bir diziye tamamıyla bir atama yapılmaz.
 
 // isimler = ['Can', 'Canan', 'Cavidan'];
 // console.log(isimler);
 //!Uncaught TypeError: Assignment to constant variable.
+
+const yaslar = [22, 18, 15];
+const kisiler = ["Ahmet", "Yilmaz", 2022, 2022 - 1970, true, 5.6, yaslar];
+console.log(kisiler);
+console.log(kisiler[6][1]);
+
+kisiler[6][2]++;
+console.log(kisiler);
+
+const sonYas = kisiler[6][2]++;
+console.log(sonYas, kisiler);
 
 
 //? ===========================================================
