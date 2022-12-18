f98dccb95f0909b0f1131a312155054297ae43d3
Onur <onurkordogan@gmail.com>
2022-12-18 16:33:47 +0300

    Session7starter

---
+++ 07_IterationMethodsinArray/1-loops.js
@@ -0,0 +1,31 @@
+//* =====================================================
+//*                     FOR LOOP
+//* ======================================================
+
+console.log('******* LOOPS IN ARRAYS ******');
+
+
+
+//* ======================================================
+//*                   FOR-IN LOOP
+//* ======================================================
+
+//*-------------------------------------------------------
+//* SORU: Ogrenciler dizisinde ogrenci isimleri saklanmaktadir.
+//* ogrncileri aramamızı sağlayacak ve aranılan ogrenciden
+//* ne kadar sayida bulunuldugunu  ana programa döndürecek bir
+//*  fonksiyonu yazınız. Eğer aranılan isimde bir ogrenci yok ise
+//* fonksiyon aranılan ogrenci bulunamdı dondurulmelidir.
+//*--------------------------------------------------------
+
+
+
+//* ======================================================
+//*                   FOR-OF LOOP
+//* ======================================================
+
+//* for of dongusu, for in dongusunun bir cok veri yapisini
+//* kapsayacak sekilde guncellenmis halidir. Bu dongude dizi
+//* icersindeki veriye erisirken indisleme kullanmaya gerek yoktur.
+
+
---
+++ 07_IterationMethodsinArray/2-iteration.js
@@ -0,0 +1,76 @@
+//* ======================================================
+//*                   FOREACH METHOD
+//* ======================================================
+
+console.log('****** ITERATION **********');
+//*Dizideki herbir fiyati konsola bastiriniz.
+
+
+
+//! Arraw function kullanılarak çözüm
+
+
+//** Fiyatlar dizisindekilerin toplamini konsola yazdiriniz */
+
+
+//! NOT: forEach metodu void bir metottur.
+//! (Yani herhangi bir değer döndürmez.)
+
+
+//*-------------- ÖRNEK -------------------
+
+
+
+//*-------------- ÖDEV -------------------
+//* fiyatlar dizisinde her bir fiyata %10 zam yapalım.
+
+//* ======================================================
+//*                       MAP METHOD
+//* ======================================================
+
+//*-------------- ÖRNEK -------------------
+//* isimler dizisin içerisindeki her ismi büyük harf olarak
+//* yeni bir diziye saklayalım.
+
+
+
+//* ======================================================
+//*                CHAINING (PIPELINE)
+//* ======================================================
+//! Tüm isimleri büyük harfe çevirip konsolda yazdiriniz.
+
+
+//* map() metodundan sonra eğer forEach gibi bir terminal(consumer) işlemi
+//* kullanılırsa yazılan ifade bir dizi döndürmemiş olur.
+
+//* ======================================================
+//*                       FILTER METHOD
+//* ======================================================
+
+//! Ahmet olanları seçip büyük harfe çevirip konsolda yazdiriniz.
+
+//* fiyatlar array'inde fiyatı 250 TL den az olanlari ayri bir diziye saklayalim.
+
+
+
+//*-------------- ÖDEV -------------------
+//* fiyatlar dizisindeki fiyatı 90'dan büyük olan değerleri
+//* konsola tek tek bastırınız.
+
+//* fiyatlar dizisindeki fiyatı 110'dan küçük olan değerlere
+//*  %10 artış yapın ve bu değerleri konsola tek tek bastırınız.
+
+//* maaslar 4000'den düsük olanlara %50 zam yapmak istiyoruz
+//* ve bunu ayri dizi olarak saklamak istiyoruz.
+
+
+//* Maasi 4000 'den büyük olanlara %25 zam yaparak sonuçlari yazdiralim.
+
+//* ======================================================
+//*                       REDUCE METHOD
+//* ======================================================
+
+//* maas toplamini bulunuz.
+
+
+//* maasi ortalamanin altinda olanlara %20 zam yapalim.
