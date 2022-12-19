fe7333236663571146f6c460db39a8c9cb9019ce
Onur <onurkordogan@gmail.com>
2022-12-19 16:40:34 +0300

    forEachFınısh

--- 07_IterationMethodsinArray/2-iteration.js
+++ 07_IterationMethodsinArray/2-iteration.js
@@ -5,19 +5,44 @@
 console.log('****** ITERATION **********');
 //*Dizideki herbir fiyati konsola bastiriniz.
 
+const fiyatlar = [100, 200, 50, 89];
 
+//*Dizideki herbir fiyati konsola bastiriniz.
 
+fiyatlar.forEach(yazdir);
+
+function yazdir(value){
+    console.log(value)
+};
+
+console.log("******************************")
 //! Arraw function kullanılarak çözüm
+
+fiyatlar.forEach((value) => console.log(value));
 
 
 //** Fiyatlar dizisindekilerin toplamini konsola yazdiriniz */
 
+let toplam = 0;
+fiyatlar.forEach((fiyat) => (toplam += fiyat));
+console.log("Toplam:", toplam)
 
 //! NOT: forEach metodu void bir metottur.
 //! (Yani herhangi bir değer döndürmez.)
+console.log(fiyatlar.forEach((fiyat) => (toplam += fiyat))); //! undefined
 
 
 //*-------------- ÖRNEK -------------------
+//! forEach 3 parametre alır, bunun için bir örnek.
+
+let toplam1 = 0;
+
+fiyatlar.forEach((deger,indis,dizi) => {
+    toplam1 += deger;
+    console.log(`${indis}.iterasyon toplamı: ${toplam1}`);
+    dizi[indis] = deger + 10;
+});
+console.log(fiyatlar);
 
 
 
